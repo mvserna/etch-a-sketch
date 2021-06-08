56336225f1e171fc035fb6e7e107cdc56b895fc6
Martin Villarreal Serna <martin.serna@gmail.com>
2021-06-07 23:17:25 -0400

    Add button to clear and request pixel dimensions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const canvas = document.getElementById('canvas');
+const button = document.getElementById('button');
 
 function drawGrid(dimension) {
   while (canvas.lastElementChild) {
@@ -10,12 +11,20 @@ function drawGrid(dimension) {
     let newDiv = document.createElement('div');
     newDiv.style.backgroundColor = 'lightblue';
     canvas.appendChild(newDiv);
-    canvas.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
-    canvas.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
-    canvas.addEventListener('mouseover', 
-      (e) => e.target.style.backgroundColor = 'black' )
+  }
+
+  canvas.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+  let gridDivs = canvas.querySelectorAll('div');
+  for ( i = 0; i < gridDivs.length; i++) {
+    gridDivs[i].addEventListener('mouseover', 
+      (e) => e.target.style.backgroundColor = 'black' );
   }
   return;
 }
+
+button.addEventListener('click', () => {
+  drawGrid( prompt('How many pixels per side?') );
+});
 
 drawGrid(16);
