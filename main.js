13bbc080caaf9118f89e93367df1e192b56823be
Martin Villarreal Serna <martin.serna@gmail.com>
2021-06-07 22:47:43 -0400

    Change background on mouseover and reset on redraw

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 
 const canvas = document.getElementById('canvas');
 
-
 function drawGrid(dimension) {
+  while (canvas.lastElementChild) {
+    canvas.removeChild(canvas.lastElementChild);
+  }
+  
   for ( i=0 ; i < (dimension*dimension) ; i++) {
     let newDiv = document.createElement('div');
     newDiv.style.backgroundColor = 'lightblue';
     canvas.appendChild(newDiv);
     canvas.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
     canvas.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+    canvas.addEventListener('mouseover', 
+      (e) => e.target.style.backgroundColor = 'black' )
   }
   return;
 }
 
+drawGrid(16);
