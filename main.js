ee1ea45317e7103b18e153b4e9257c130b57ab56
Martin Villarreal Serna <martin.serna@gmail.com>
2021-06-09 16:09:07 -0400

    Set random hue for each grid div when generated

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 
 const canvas = document.getElementById('canvas');
 const button = document.getElementById('button');
-let lightness = [""]; 
 
 function paint(e) {
   let targetLightness = e.target.getAttribute('data-lightness');
-  let randomHue = Math.random() * 360;
-  e.target.style.backgroundColor = `hsl(${randomHue}, 100%, ${targetLightness}%)`;
+  let targetHue = e.target.getAttribute('data-hue');
+  e.target.style.backgroundColor = `hsl(${targetHue}, 100%, ${targetLightness}%)`;
   e.target.setAttribute('data-lightness', `${targetLightness - 10}`)
 }
 
@@ -16,10 +15,10 @@ function drawGrid(dimension) {
   }
   
   for ( i=0 ; i < (dimension*dimension) ; i++) {
+    let randomHue = Math.random() * 360;
     let newDiv = document.createElement('div');
-    newDiv.setAttribute('id', `${i}`);
     newDiv.setAttribute('data-lightness', '100');
-    lightness.push(100);
+    newDiv.setAttribute('data-hue', `${randomHue}`)
     newDiv.style.backgroundColor = 'lightblue';
     canvas.appendChild(newDiv);
   }
